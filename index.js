7c5914c5098b2129461a2b8613786b5b3854b3d7
vaibhav27996 <vmahajan27996@gmail.com>
2022-12-18 17:29:03 +0530

    setup static file access layout my project

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express=require('express');
 const port=8000;
-
 const app=express();
+const expressLayouts=require('express-ejs-layouts');
+
+app.use(expressLayouts);
+app.use(express.static('./assets'));
+
+//extract style and script from sub page into the layout
+app.set("layout extractStyles",true);
+app.set("layout extractScripts",true);
 
 //use express router
 app.use('/',require('./routes/index'));
 app.use('/user',require('./routes/user' ));
 
+//set up the view engine
 app.set('view engine','ejs');
 app.set('views','./views');
+
+app.use(express.static('assets'));
 
 app.listen(port,function(err){
     if(err){
