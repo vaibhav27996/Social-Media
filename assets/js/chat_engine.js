c0eeb474cb353e72506e62c1f471f35169eb57d7
vaibhav27996 <vmahajan27996@gmail.com>
2023-01-14 11:45:36 +0530

    last comment

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -4,7 +4,7 @@ class ChatEngine{
         this.userEmail = userEmail;
         this.userName = userName;
 
-        this.socket = io.connect('http://localhost:5000');
+        this.socket = io.connect('http://52.91.126.77:5000');
 
         if (this.userEmail){
             this.connectionHandler();
